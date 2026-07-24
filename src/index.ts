f00023fe931f40e57b93d0fd8bfddd38b819562c
Neykoor <cafecafirexos@gmail.com>
2026-07-24 02:09:08 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,13 @@
 import { Readable } from 'stream';
-import { FfmpegCommand, FfmpegCommandOptions } from './FfmpegCommand';
-
-function ffmpeg(input?: string | Readable, options?: FfmpegCommandOptions): FfmpegCommand {
-  return new FfmpegCommand(input, options);
-}
-
-export { FfmpegCommand, FfmpegCommandOptions };
-export {
-  WebpStickerOptions,
-  GifOptions,
-  WatermarkOptions,
-  SilenceTrimOptions
-} from './FfmpegCommand';
-export { ffprobe, setFfprobePath, FfprobeData, FfprobeFormat, FfprobeStream } from './ffprobe';
-export { ProgressInfo } from './progress';
-export { getMediaInfo, MediaInfo } from './mediaInfo';
-export { detectExtension } from './fileSignature';
-export { createTempFile, removeTempFile } from './tempFile';
-export { checkFfmpegInstalled, HealthcheckResult } from './healthcheck';
-export { concatBuffers, ConcatBuffersOptions } from './concat';
-export {
+import { FfmpegCommand, FfmpegCommandOptions, setFfmpegPath } from './FfmpegCommand';
+import { ffprobe, setFfprobePath, FfprobeData, FfprobeFormat, FfprobeStream } from './ffprobe';
+import { ProgressInfo } from './progress';
+import { getMediaInfo, MediaInfo } from './mediaInfo';
+import { detectExtension } from './fileSignature';
+import { createTempFile, removeTempFile } from './tempFile';
+import { checkFfmpegInstalled, HealthcheckResult } from './healthcheck';
+import { concatBuffers, ConcatBuffersOptions } from './concat';
+import {
   convertBuffer,
   ConvertBufferOptions,
   bufferToWebp,
@@ -33,4 +21,46 @@ export {
   bufferToThumbnailJpeg,
   BufferToThumbnailOptions
 } from './convert';
-export default ffmpeg;
+
+function ffmpeg(input?: string | Readable, options?: FfmpegCommandOptions): FfmpegCommand {
+  return new FfmpegCommand(input, options);
+}
+
+ffmpeg.FfmpegCommand = FfmpegCommand;
+ffmpeg.setFfmpegPath = setFfmpegPath;
+ffmpeg.ffprobe = ffprobe;
+ffmpeg.setFfprobePath = setFfprobePath;
+ffmpeg.getMediaInfo = getMediaInfo;
+ffmpeg.detectExtension = detectExtension;
+ffmpeg.createTempFile = createTempFile;
+ffmpeg.removeTempFile = removeTempFile;
+ffmpeg.checkFfmpegInstalled = checkFfmpegInstalled;
+ffmpeg.concatBuffers = concatBuffers;
+ffmpeg.convertBuffer = convertBuffer;
+ffmpeg.bufferToWebp = bufferToWebp;
+ffmpeg.bufferToWebpSized = bufferToWebpSized;
+ffmpeg.bufferToWhatsappAudio = bufferToWhatsappAudio;
+ffmpeg.bufferToWhatsappVideo = bufferToWhatsappVideo;
+ffmpeg.bufferToThumbnailJpeg = bufferToThumbnailJpeg;
+
+export = ffmpeg;
+
+declare namespace ffmpeg {
+  export {
+    FfmpegCommand,
+    FfmpegCommandOptions,
+    FfprobeData,
+    FfprobeFormat,
+    FfprobeStream,
+    ProgressInfo,
+    MediaInfo,
+    HealthcheckResult,
+    ConcatBuffersOptions,
+    ConvertBufferOptions,
+    BufferToWebpOptions,
+    BufferToWebpSizedOptions,
+    BufferToWhatsappAudioOptions,
+    BufferToWhatsappVideoOptions,
+    BufferToThumbnailOptions
+  };
+}
