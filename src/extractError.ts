6c6ca383f7f75914a9668d481ab72ef9e3f5f267
Neykoor <cafecafirexos@gmail.com>
2026-07-23 17:49:16 -0700

    Update extractError.ts

--- src/extractError.ts
+++ src/extractError.ts
@@ -1,14 +1,19 @@
 const NEWLINE_REGEXP = /\r\n|\r|\n/;
 
 export function extractError(stderr: string): string {
-  return stderr
-    .split(NEWLINE_REGEXP)
-    .reduce<string[]>((messages, message) => {
-      if (message.charAt(0) === ' ' || message.charAt(0) === '[') {
-        return [];
+  const lines = stderr.split(NEWLINE_REGEXP);
+  let messages: string[] = [];
+
+  for (const line of lines) {
+    if (line.charAt(0) === ' ' || line.charAt(0) === '[') {
+      if (messages.length) {
+        messages.push(line);
       }
-      messages.push(message);
-      return messages;
-    }, [])
-    .join('\n');
+      continue;
+    }
+
+    messages = [line];
+  }
+
+  return messages.join('\n');
 }
